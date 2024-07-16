1cf0d7db18d40bf05cfbc8b71813e25ac24c212e
aleksfolt <122365476+aleksfolt@users.noreply.github.com>
2024-07-16 04:52:11 +0300

    Create exchange_module.py

---
+++ exchange_module.py
@@ -0,0 +1,34 @@
+from pyrogram import Client, filters
+import requests
+
+with open("Yuki.bot", "r") as file:
+    data = {}
+    for line in file:
+        key, value = line.strip().split('=')
+        data[key] = value
+    prefix_userbot = data['prefix']
+    OWNER_ID = int(data['user_id'])
+    print(OWNER_ID)
+
+cinfo = f"💱`{prefix_userbot}exchange_rate`"
+ccomand = " показывает курс обмена валют. Пример: `exchange_rate USD RUB`"
+
+def is_owner(_, __, message):
+    return message.from_user.id == OWNER_ID
+
+def register_module(app: Client):
+    @app.on_message(filters.create(is_owner) & filters.command("exchange_rate", prefixes=prefix_userbot))
+    async def exchange_rate(_, message):
+        try:
+            base, target = message.text.split(" ")[1:3]
+            response = requests.get(f"https://api.exchangerate-api.com/v4/latest/{base.upper()}")
+            data = response.json()
+            rate = data["rates"].get(target.upper())
+            if rate:
+                reply_text = f"**Курс {base.upper()} к {target.upper()}:** {rate}"
+            else:
+                reply_text = "**❌Неверная валюта.**"
+        except Exception as e:
+            reply_text = f"**❌Произошла ошибка: {e}**"
+        await message.delete()
+        await message.reply_text(reply_text)
