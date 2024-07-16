1d7417149610088e7e46ae2ba0d82da1a65c98da
aleksfolt <122365476+aleksfolt@users.noreply.github.com>
2024-07-16 19:11:14 +0300

    Create gpt_module.py

---
+++ gpt_module.py
@@ -0,0 +1,45 @@
+from pyrogram import Client, filters
+import g4f
+
+g4f.debug.logging = False
+g4f.check_version = False
+
+with open("Yuki.bot", "r") as file:
+    lines = file.readlines()
+    prefix_userbot = lines[2].strip().split('=')[1]
+    OWNER_ID = int(lines[1].strip().split('=')[1])
+
+cinfo = f"🧠`{prefix_userbot}gpt`"
+ccomand = " ChatGPT"
+
+def load_blocklist(filename):
+    with open(filename, "r") as file:
+        return [line.strip() for line in file.readlines()]
+
+blocklist = load_blocklist("blocklist.info")
+
+def contains_blocked_word(text):
+    for word in blocklist:
+        if word in text:
+            return True
+    return False
+
+def replace_blocked_words(text):
+    for word in blocklist:
+        text = text.replace(word, "DELETED")
+    return text
+
+def is_owner(_, __, message):
+    return message.from_user.id == OWNER_ID
+
+def register_module(app: Client):
+    @app.on_message(filters.create(is_owner) & filters.command("gpt", prefixes=prefix_userbot))
+    async def gpt_command(_, message):
+    user_input = message.text.split(f"{prefix_userbot}gpt ", maxsplit=1)[1]
+        response = g4f.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": user_input}],
+        )
+        response_text = response["choices"][0]["message"]["content"]
+        reply_text = f"**👨Ваш запрос: {user_input}**\n🧠Ответ от ChatGPT: `{response_text}`"
+        await message.reply_text(reply_text)
