e1bdde1d0e0d65608895fede7b2ea3ef0c6de0c9
aleksfolt <122365476+aleksfolt@users.noreply.github.com>
2024-07-20 03:44:42 +0300

    Create spam.py

---
+++ spam.py
@@ -0,0 +1,33 @@
+from pyrogram import Client, filters
+import asyncio
+
+with open("Yuki.bot", "r") as file:
+    data = {}
+    for line in file:
+        key, value = line.strip().split('=')
+        data[key] = value
+    prefix_userbot = data['prefix']
+    OWNER_ID = int(data['user_id'])
+
+cinfo = f"📝`{prefix_userbot}spam`"
+ccomand = f" отправляет сообщение указанное количество раз с заданным интервалом. Пример: `{prefix_userbot}spam 10 привет 0.1`"
+
+def register_module(app: Client):
+    @app.on_message(filters.me & filters.command("spam", prefixes=prefix_userbot))
+    async def spam(_, message):
+        try:
+            parts = message.text.split(" ", maxsplit=3)
+            count = int(parts[1])
+            text = parts[2]
+            delay = float(parts[3])
+
+            for _ in range(count):
+                await message.reply_text(text)
+                await asyncio.sleep(delay)
+
+        except IndexError:
+            await message.reply_text("**❌ Неправильный формат команды. Пример: `.spam 10 привет 0.1`**")
+        except ValueError:
+            await message.reply_text("**❌ Убедитесь, что количество сообщений и задержка указаны правильно.**")
+        except Exception as e:
+            await message.reply_text(f"**❌ Произошла ошибка: {e}**")
