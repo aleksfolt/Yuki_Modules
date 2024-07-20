49c026893b1bdc03ecf7c4b1e1e2d77b30389c6e
aleksfolt <122365476+aleksfolt@users.noreply.github.com>
2024-07-20 03:58:54 +0300

    Update userinfo.py

--- userinfo.py
+++ userinfo.py
@@ -6,7 +6,6 @@ CONFIG_FILE = "config.json"
 with open(CONFIG_FILE, "r") as file:
     config_data = json.load(file)
     prefix_userbot = config_data['prefix']
-    OWNER_ID = int(config_data['user_id'])
 
 cinfo = f"☀{prefix_userbot}userinfo"
 ccomand = " показывает информацию о пользователе"
